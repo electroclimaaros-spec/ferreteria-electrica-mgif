f6fedd8b5838774d31367079e0b602d78d770715
electroclimaaros-spec <electroclima.aros@gmail.com>
2026-06-18 02:19:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const botones = document.querySelectorAll(".categorias button");
+const tarjetas = document.querySelectorAll(".card");
 
+botones.forEach(boton => {
+
+    boton.addEventListener("click", () => {
+
+        const filtro = boton.dataset.filtro;
+
+        tarjetas.forEach(card => {
+
+            if (card.dataset.categoria === filtro) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
