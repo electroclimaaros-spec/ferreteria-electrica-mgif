9cffacc4a2b5be7b2cdc49526fa1543925f7ad56
electroclimaaros-spec <electroclima.aros@gmail.com>
2026-06-18 16:00:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,9 @@
-const botonesCarrito = document.querySelectorAll(".card .carrito");
-const listaCarrito = document.getElementById("lista-carrito");
-const totalTexto = document.getElementById("total");
-
+let cantidad = 0;
 let total = 0;
-
-function actualizarTotal() {
-    totalTexto.innerText = total.toLocaleString("es-CL");
+function agregarProducto(precio){
+    cantidad++;
+    total += precio;
+    document.getElementById("cantidad").innerText = cantidad;
+    document.getElementById("total").innerText =
+        total.toLocaleString("es-CL");
 }
-
-botonesCarrito.forEach((boton) => {
-
-    boton.addEventListener("click", () => {
-
-        const tarjeta = boton.parentElement;
-
-        const nombre = tarjeta.querySelector("h3").innerText;
-
-        let precioTexto = tarjeta.querySelector("p").innerText;
-
-        // Eliminar $ y puntos
-        precioTexto = precioTexto
-            .replace(/\$/g, "")
-            .replace(/\./g, "")
-            .trim();
-
-        const precio = parseInt(precioTexto);
-
-        if (isNaN(precio)) {
-            console.error("Precio inválido:", precioTexto);
-            return;
-        }
-
-        // Crear elemento del carrito
-        const item = document.createElement("li");
-
-        item.innerHTML = `
-            ${nombre} - $${precio.toLocaleString("es-CL")}
-            <button class="eliminar">❌</button>
-        `;
-
-        listaCarrito.appendChild(item);
-
-        total += precio;
-        actualizarTotal();
-
-        // Botón eliminar
-        const botonEliminar = item.querySelector(".eliminar");
-
-        botonEliminar.addEventListener("click", () => {
-
-            total -= precio;
-
-            if (total < 0) {
-                total = 0;
-            }
-
-            item.remove();
-
-            actualizarTotal();
-
-        });
-
-    });
-
-});
