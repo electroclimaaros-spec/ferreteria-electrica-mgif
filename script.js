83055fe4894677acfb8a4f53a52a4daaf37eadae
electroclimaaros-spec <electroclima.aros@gmail.com>
2026-06-18 16:02:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let cantidad = 0;
 let total = 0;
-function agregarProducto(precio){
+
+function agregarProducto(precio) {
+
     cantidad++;
+
     total += precio;
+
     document.getElementById("cantidad").innerText = cantidad;
+
     document.getElementById("total").innerText =
         total.toLocaleString("es-CL");
+
 }
